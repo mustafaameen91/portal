15d2e5043f33a0fc97f49e0b77734abf76ebd39c
mustafaameen91 <mustafa.mohammed.ameen91@gmail.com>
2021-07-27 10:18:59 +0300

    new password

--- app/controllers/newMasterSheet.controllers.js
+++ app/controllers/newMasterSheet.controllers.js
@@ -9,7 +9,7 @@ let transporter = nodemailer.createTransport(
       port: 465,
       auth: {
          user: "results5@duc.edu.iq",
-         pass: "20032004",
+         pass: "Rr123123",
       },
    })
 );
